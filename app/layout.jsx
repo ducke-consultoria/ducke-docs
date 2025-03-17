031e521dd2eb4b2597410afec6cf4c1b15de31e7
Raphael Zappacosta <40356907+raphazappa@users.noreply.github.com>
2025-03-17 11:55:21 -0300

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,20 +8,24 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const banner = <Banner storageKey="ducke">
+    <a href="https://crm.ducke.com.br" target="_blank">
+      🎉 Conheça já o Ducke CRM! Saiba mais →
+    </a>
+  </Banner>
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<img src="/ducke-logo.png" alt="Ducke" width={64} height={32} />}
     // ... Your additional navbar options
   />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+const footer = <Footer>Powered by 2023-{new Date().getFullYear()} © Ducke.</Footer>
 
 export default async function RootLayout({ children }) {
   return (
     <html
       // Not required, but good for SEO
-      lang="en"
+      lang="pt-br"
       // Required to be set
       dir="ltr"
       // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
@@ -37,7 +41,7 @@ export default async function RootLayout({ children }) {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://docs.ducke.com.br"
           footer={footer}
           // ... Your additional layout options
         >
